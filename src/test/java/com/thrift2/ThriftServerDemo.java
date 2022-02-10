61c769b207236dc91034b7b004a9728b7760213d
huozheng1995 <339078795@qq.com>
2022-02-10 10:00:43 +0800

    add thrift

---
+++ src/test/java/com/thrift2/ThriftServerDemo.java
@@ -0,0 +1,29 @@
+package com.thrift2;
+
+import org.apache.thrift.TProcessorFactory;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.transport.layered.TFramedTransport;
+
+public class ThriftServerDemo {
+  private final static int DEFAULT_PORT = 30001;
+  private static TServer server = null;
+
+  public static void main(String[] args) {
+    try {
+      TNonblockingServerSocket socket = new TNonblockingServerSocket(DEFAULT_PORT);
+      TestQry.Processor processor = new TestQry.Processor(new QueryImp());
+      TNonblockingServer.Args arg = new TNonblockingServer.Args(socket);
+      arg.protocolFactory(new TBinaryProtocol.Factory());
+      arg.transportFactory(new TFramedTransport.Factory());
+      arg.processorFactory(new TProcessorFactory(processor));
+      server = new TNonblockingServer(arg);
+      server.serve();
+    } catch (TTransportException e) {
+      e.printStackTrace();
+    }
+  }
+}
